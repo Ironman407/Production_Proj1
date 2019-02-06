5efa1ffb2cc639654405873d558f2f813c312b2b
Ironman407 <42649627+Ironman407@users.noreply.github.com>
2019-02-06 12:28:25 -0500

    Add files via upload
    Base Code for player movement and camera control

---
+++ GameController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] guards;
+    public GameObject[] lights;
+    public GameObject guard;
+    public GameObject Player;
+    public Transform guardSpawn;
+    public PlayerController playerController;
+    public int guardCount;
+    private float spawnWait;
+    private float startWait;
+    int i;
+
+    private bool gameOver;
+    
+
+    void Awake ()
+    {
+        playerController = Player.GetComponent<PlayerController>();
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        Debug.Log(playerController.hasItem);
+    }
+
+    
+    
+    void SpawnGuard ()
+    {
+        Debug.Log("Spawn Guards.");
+        while (guardCount < 5)
+        {
+            Debug.Log("Doesn't Spawn Guards.");
+            Instantiate(guard, guardSpawn);
+            guardCount++;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerController.hasItem == true)
+        {
+            Debug.Log("Spawn Them1.");
+            InvokeRepeating("SpawnGuard", 1f, 3f);
+        }
+    }
+
+}
---
+++ PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+   // public float gravity = -9.8f;
+    public GameController gameController;
+
+    
+    private Rigidbody rb;
+   
+    public bool hasItem;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        hasItem = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //Jump
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+            // Collects Amulet and sets bool to true
+           if(other.gameObject.CompareTag ("Amulet"))
+           {
+            other.gameObject.SetActive(false);
+            hasItem = true;
+           }
+    }
+
+    void FixedUpdate()
+    {
+        //Basic Movement
+        float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+        float moveVertical = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        movement = Vector3.ClampMagnitude(movement, speed); //Limits max speed of player
+        //movement *= Time.deltaTime;
+        //movement.y = gravity;
+        movement = transform.TransformDirection(movement);
+        rb.AddForce(movement * speed);
+    }
+}
